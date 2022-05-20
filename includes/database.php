59c6a55005143b04760532a4a05ad798a6a8f076
Mike Brown <mikeabrown2020@gmail.com>
2022-05-20 10:37:29 -0400

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -3,11 +3,5 @@
 	$username = "honorFlight";
 	$password = "3RM)NzAF(_lV[VAv";
 	$databaseName = "2022_honor_flight"
-	
-	$conn = new mysqli ($hostname, $userName, $password, $databaseName);
-	
-	//check connection
-	if ($conn-> connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+
 ?>
