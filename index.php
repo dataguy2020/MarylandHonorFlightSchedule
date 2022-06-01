ff55eabbdf4bbda2dcd6171c99f54c7a7c515e32
Mike Brown <mikeabrown2020@gmail.com>
2022-06-01 13:00:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,33 +11,61 @@
    <body>
       <script src="index.js"></script>
       <table class="table">
-         <caption>Schedule Table</caption>
+         <!--<caption>Schedule Table</caption>-->
          <thead>
             <tr>
-               <th scope="col">#</th>
-               <th scope="col">First</th>
-               <th scope="col">Last</th>
-               <th scope="col">Handle</th>
+               <th scope="col">ID</th>
+               <th scope="col">Arrival Date</th>
+               <th scope="col">Arrival Day</th>
+               <th scope="col">Hub</th>
+               <th scope="col">Departing A/P</th>
+               <th scope="col">Arriving A/P</th>
+               <th scope="col">Flight Type</th>
+               <th scope="col">Arrival Time</th>
+               <th scope="col"Arrival Carrier/th>
+               <th scope="col">Arrival Flight #</th>
+               <th scope="col">Arrival Gate</th>
+               <th scope="col">Deaprting Date</th>
+               <th scope="col">Departing Day</th>
+               <th scope="col">Return A/P</th>
+               <th scope="col">Depart Carrier</th>
+               <th scope="col">Depart Flight #</th>
+               <th scope="col">Departing Time</th>
+               <th scope="col"># of Vets</th>
+               <th scope="col"># of Guardians</th>
+               <th scope="col"># of W/C</th>
+               <th scope="col">Bus Company</th>
+               <th scope="col"># of Busses</th>
+               <th scope="col">Status</th>
+               <th scope="col">Updated</th>
             </tr>
          </thead>
          <tbody>
-            <tr>
+             <tr>
                <th scope="row">1</th>
                <td>Mark</td>
                <td>Otto</td>
                <td>@mdo</td>
-            </tr>
-            <tr>
-               <th scope="row">2</th>
-               <td>Jacob</td>
-               <td>Thornton</td>
-               <td>@fat</td>
-            </tr>
-            <tr>
-               <th scope="row">3</th>
-               <td>Larry</td>
-               <td>the Bird</td>
-               <td>@twitter</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
+               <td>@mdo</td>
             </tr>
          </tbody>
       </table>
